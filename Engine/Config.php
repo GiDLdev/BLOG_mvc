ed5debcd637ab22071a686c14b37a02691d0746e
GiDLdev <contact.gildas@free.fr>
2021-02-04 16:57:38 +0100

    mise à jour bdd config readme

--- Engine/Config.php
+++ Engine/Config.php
@@ -9,7 +9,7 @@ final class Config
     DB_HOST = 'localhost',
     DB_NAME = 'blog-ecrivain',
     DB_USR = 'root',
-    DB_PWD = '',
+    DB_PWD = 'root',
 
     // Titre du site
     SITE_NAME = 'Billet simple pour l\'Alaska';
